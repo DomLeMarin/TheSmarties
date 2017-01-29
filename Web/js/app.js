c0b6a9708072d8c8e48b82810d8025c1f8081ba9
DomLeMarin <christianraemy@gmail.com>
2017-01-29 04:38:21 +0100

    Refactoring of the web-interface to fit the new solidity contract, still much work to be done... aie aie aie

--- Web/js/app.js
+++ Web/js/app.js
@@ -4,7 +4,7 @@
 	///Insert your contract address var here:
 	var contractAddress = '0xed6774fa1458dc34ac482c1f8d8fc2072e5749e5';
 	var contractABI = [{"constant":false,"inputs":[],"name":"startMonitoring","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"location","type":"string"}],"name":"indexForLocation","outputs":[{"name":"index","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"myid","type":"bytes32"},{"name":"result","type":"string"}],"name":"__callback","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"myid","type":"bytes32"},{"name":"result","type":"string"},{"name":"proof","type":"bytes"}],"name":"__callback","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"useDelay","type":"bool"}],"name":"update","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"location","type":"string"},{"name":"URL","type":"string"}],"name":"addSensor","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"getMap","outputs":[],"payable":true,"type":"function"},{"constant":false,"inputs":[{"name":"sensor","type":"uint256"}],"name":"getPollution","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"helloWorld","outputs":[],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"location","type":"string"}],"name":"getPollution","outputs":[],"payable":false,"type":"function"},{"inputs":[],"payable":true,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"description","type":"string"}],"name":"newValueQuery","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"value","type":"string"}],"name":"Log","type":"event"}];
- 	
+ 	var continuousMonitoring = false;
 	
 	function init() {
 
@@ -43,7 +43,28 @@
 				document.getElementById('logging').innerHTML += "<p><small>"+result.args.value+"</small></p>";
 			}
 		}
-	);	
+	);
+	
+	var pollutionAlert = contract.PollutionAlert({},{fromBlock:block-100, toBlock:50000});
+	pollutionAlert.watch(
+		function(error,result) {
+			if (error) {
+				alert("Error: " + error);
+			} else {
+				document.getElementById('logging').innerHTML += "<p> Pollution Alert in "+result.args.value+"</p>";
+				if (result.args.value == "west") {
+					$('.stately li#ca').css({"color":"#FF0000"});
+				}
+				if (result.args.value == "middle") {
+					$('.stately li#tx').css({"color":"#FF0000"});
+				}
+				if (result.args.value == "east") {
+					$('.stately li#ny').css({"color":"#FF0000"});
+				}				
+			}
+		}
+	);
+		
 				
 		
 	}
@@ -53,30 +74,48 @@
 	}
 
 	function addSensor() {
-		var sensor = document.getElementById("addSensor").value;
-		$('.stately li#ca').css({"color":"#000000"});
+		var location = document.getElementById("addSensorLocation").value;
+		var URL = document.getElementById("addSensorURL").value;
 		
-		contract.addSensor.sendTransaction({sensor,sensor},
+		contract.addSensor.sendTransaction({location,URL},
 			{from:web3.eth.defaultAccount, gas:100000}, function(err, res) {if (err) alert(err); else alert(res);}
 		);
 	};
 
 	function getSensor() {
 		var sensor = document.getElementById("getSensor").value;
-		contract.addSensor(sensor).sendTransaction(
+		contract.addSensor.sendTransaction(sensor,
 			{from:web3.eth.defaultAccount, gas:100000}, function (error, result){ 
 					alert("You are getting a sensor");
 			}
 		);
 	};;
 
-	function vote() {
-        var vote = document.getElementById("getAveragePollution").value;
-		contract.vote(vote).sendTransaction(
+	function getAveragePollution() {
+        var region = document.getElementById("getAveragePollution").value;
+		contract.getAveragePollution.sendTransaction(region,
 			{from:web3.eth.defaultAccount, gas:100000}, function (error, result){ 
 				alert("You are getting average pollution");
 			}
 		);	
+	}
+	
+	function updateMap() {
+		continuousMonitoring = !continuousMonitoring;
+		if (continuousMonitoring) {
+			document.getElementById('updateMap').innerText = "Continuously loading...";
+			contract.startMonitoring.sendTransaction(
+				{from:web3.eth.defaultAccount, gas:100000}, function(err, res) {if (err) alert(err); else alert(res);}
+			);
+			
+		} 
+		else {
+			document.getElementById('updateMap').innerText = "";
+			contract.stopMonitoring.sendTransaction(
+				{from:web3.eth.defaultAccount, gas:100000}, function(err, res) {if (err) alert(err); else alert(res);}
+			);
+		}
+		
 	}
 		
 				
